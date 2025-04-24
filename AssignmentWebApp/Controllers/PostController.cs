0e3eebe900cac1744ae2a62fd98ed04ddf13fe9e
AkashSalvi <akashsalvi8676@gmail.com>
2025-04-24 22:37:11 +0530

    feature/TRAINING-2 : Created a Post page to display posts and comments

---
+++ AssignmentWebApp/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using AssignmentWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentWebApp.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index(int id = 1)
+        {
+            var post = new PostViewModel
+            {
+                Id = 1,
+                Title = "What is ASP.NET Core",
+                Author = "Shailendra Chauhan",
+                Body = "ASP.NET Core is an open-source framework..."
+            };
+            return View(post);
+        }
+    }
+}
