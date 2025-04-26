e9cae4af98f55548640c9fceb7dc166595c20f35
AkashSalvi <akashsalvi8676@gmail.com>
2025-04-26 19:44:29 +0530

    feature/TRAINING-3 : Added Tag helper command button with submitting behaviour.

---
+++ AssignmentWebApp/TagHelpers/ActiveLinkTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace AssignmentWebApp.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "asp-active-link")]
+    public class ActiveLinkTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var currentController = ViewContext.RouteData.Values["controller"]?.ToString();
+            var currentAction = ViewContext.RouteData.Values["action"]?.ToString();
+
+            if (string.Equals(currentController, Controller, StringComparison.OrdinalIgnoreCase) &&
+                string.Equals(currentAction, Action, StringComparison.OrdinalIgnoreCase))
+            {
+                var existingClasses = output.Attributes["class"]?.Value?.ToString() ?? "";
+                output.Attributes.SetAttribute("class", $"{existingClasses} active".Trim());
+            }
+
+            output.Attributes.RemoveAll("asp-active-link");
+        }
+    }
+}
---
+++ AssignmentWebApp/TagHelpers/CommandButtonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AssignmentWebApp.TagHelpers
+{
+    [HtmlTargetElement("command-button")]
+    public class CommandButtonTagHelper:TagHelper
+    {
+        public string Type { get; set; } = "submit";
+        public string Class { get; set; } = "btn btn-primary";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("type", Type);
+            output.Attributes.SetAttribute("class", Class);
+            if (output.Content.IsEmptyOrWhiteSpace)
+            {
+                output.Content.SetContent("Submit");
+            }
+        }
+    }
+}
